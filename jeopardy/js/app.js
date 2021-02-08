2a506a0217fb9c86fd846580092ba2ecca16a241
jessicagallagher <jessicalyngallagher@me.com>
2021-02-08 18:43:55 -0500

    show hide modal function written but not tested

--- jeopardy/js/app.js
+++ jeopardy/js/app.js
@@ -1,13 +1,30 @@
+/*
+i heavily relied on the following sources to help using bootstrap modals with varying modal content since this was my first time doing this:
+
+1. https://getbootstrap.com/docs/5.0/components/modal/
+2. https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes
+3. https://jsbin.com/atawaz/3/edit?html,output
+
+*/
+
 $(() => {
 
-	// global variable
 	let score = 0;
 
-	// getting hints from array
-
-
-
-
-
-
+	// show and hide modal
+	const showHideModal =
 });
+
+
+const showHideModal = () => {
+	$('#myModal').on('show.bs.modal', (e) => {
+		giveHint(e, $(e.currentTarget))
+	});
+};
+$('#myModal').on('hidden.bs.modal', () => {
+	console.log('hidden');
+});
+$('.modal-body input:checked').submit() => {
+	$('#myModal').modal('toggle');
+	return false;
+};
