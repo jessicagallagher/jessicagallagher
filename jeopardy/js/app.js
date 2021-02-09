2e58a7785da8128b54b8071ecace5037860683dd
jessicagallagher <jessicalyngallagher@me.com>
2021-02-09 09:09:31 -0500

    fixed rows and columns

--- jeopardy/js/app.js
+++ jeopardy/js/app.js
@@ -7,50 +7,6 @@ i heavily relied on the following sources to help using bootstrap modals with va
 
 */
 
-$(() => {
-
-	let score = 0;
-
-	// show and hide modal
-
-});
 
 
-const giveChoices = (theHint) => {
-	let $btnDiv = $('<div id="disabled" class="btn-group" data-toggle="buttons"></div>');
-	theHint.answers.forEach = (ans) => {
-		let $div = $('<div class="radio">');
-		let $label = $('<label class="radio-inline"></label>');
-		let $input = $('<input type="radio" name="ans" value="' + ans + '">');
-		$label.append($input);
-		$label.append(ans);
-		$div.append($label);
-		$btnDiv.append($div);
-	};
-	return $btnDiv;
-}
-
-
-const giveHint = (e, $modal) => {
-	let clickedButton = $(e.currentTarget);
-	let num = parseInt(clickedButton.data('num'));
-	let theHint = hints[num];
-	window.currentHint = num;
-	$modal.find('.modal-title').text(theHint.hint);
-	$modal.find('.modal-body').empty().append(giveChoices(theHint));
-}
-
-
-
-const showHideModal = () => {
-	$('#myModal').on('show.bs.modal', (e) => {
-		giveHint(e, $(e.currentTarget))
-	});
-};
-$('#myModal').on('hidden.bs.modal', () => {
-	console.log('hidden');
-});
-$('.modal-body input:checked').submit() => {
-	$('#myModal').modal('toggle');
-	return false;
-};
+/* jquery attempt */
