ac43099e679a52b98a6e1109d9393b9226c168b5
jessicagallagher <jessicalyngallagher@me.com>
2021-02-10 22:37:49 -0500

    made some huge errors and rewrote everything. it works but i still can't get the modal to close.

--- jeopardy/js/app.js
+++ jeopardy/js/app.js
@@ -5,7 +5,7 @@
 3. https://jsbin.com/atawaz/3/edit?html,output */
 
 /*
-[] open modal on click
+[x] open modal on click
   - modal should then populate with the hint and the four answer options
 
 [] close modal on submission of answer
@@ -16,10 +16,155 @@
   - if no, subtract point value to score
   - determine what the winning conditions are since the player isn't playing against the computer. should they answer ALL 30 questions?
 
-[] have score update in real-time
+[x] have score update in real-time
 
 [] have a button to reset the entire game
 */
+
+//==============================================
+// variables for dynamic modal text updates
+// initiates game play by populating modals with
+// questions and answer options
+//==============================================
+
+let clueText = document.getElementById('clueText');
+let cluePoints = document.getElementById('points');
+let answer1 = document.getElementById('answer1');
+let answer2 = document.getElementById('answer2');
+let answer3 = document.getElementById('answer3');
+let answer4 = document.getElementById('answer4');
+let points = document.getElementById('points');
+
+//==============================================
+// this allows the score to update in the header
+//==============================================
+
+let updateScore = document.querySelector('.updateScore')
+
+//========================
+// game-specific variables
+//========================
+
+let clickedHint; // this variable will change because it stands for the actual gameboard piece that was clicked
+let currentScore = 0; // this is the true current score of the game at the start
+let hintPoints = 0; // this is the score of the clicked hints
+
+//==========
+// functions
+//==========
+
+// opens modal on click
+const revealHint = (cluenames, clue) => {
+	// this function has to be called here because showing the modal isn't a function
+	populateModal(cluenames, clue);
+	$('#clueModal').modal('show');
+	// console.log('clicked'); just making sure it works
+	// console.log($(event.currentTarget)); making sure i'm on the right track for the next function
+}
+
+
+// populate modal information dynamically
+const populateModal = (cluenames, clue) => {
+	// this is the easiest way to generate this on the modal. the values increase by 200.
+	points.innerHTML = '$' + 200 * clue;
+	// this is pulling the data from the key value pairs based on the "click coordinates"
+	clueText.innerHTML = hints['cluenames' + cluenames]['clue' + clue].clueText;
+	answer1.innerHTML = hints['cluenames' + cluenames]['clue' + clue].answer1;
+	answer2.innerHTML = hints['cluenames' + cluenames]['clue' + clue].answer2;
+	answer3.innerHTML = hints['cluenames' + cluenames]['clue' + clue].answer3;
+	answer4.innerHTML = hints['cluenames' + cluenames]['clue' + clue].answer4;
+	// this is assigning the correct answer based on the switch statement in the function
+	findCorrectAnswer(hints['cluenames' + cluenames]['clue' + clue].correct);
+	// assign the point value for the clicked hint --> we will need this to tally the score later
+	hintPoints = event.target.innerHTML;
+	// console.log(hintPoints); making sure the value is correct --> we will need to remove the $ later so that it can be an integer
+	clickedHint = event.target;
+}
+
+// assign the correct answer which will be called above so that we don't get undefined
+const findCorrectAnswer = (correct) => {
+	// using switch statement to compare the value of the expression to the value of each case(aka trying to find a match between correct and the answer option)--break means there wasn't a match so move on: https://www.w3schools.com/js/js_switch.asp
+	switch (correct) {
+	case '1':
+		answer1.classList.add('correct');
+		break;
+
+	case '2':
+		answer2.classList.add('correct');
+		break;
+
+	case '3':
+		answer3.classList.add('correct');
+		break;
+
+	case '4':
+		answer4.classList.add('correct');
+		break;
+	default:
+		console.log('help')
+	}
+
+}
+
+// when the answer is submitted, we will want to disable the button so it can't be clicked again. we will call that function within the answer submission or else we will get undefined
+const disableButton = (event) => {
+	event.classList.remove('clue');
+	event.classList.add('disabled');
+	event.innerHTML = '';
+}
+
+// at this point, all testing of each code block gives an error that is based on the onclick from the html, so we are on the right track
+
+const finalAnswer = () => {
+	disableButton(clickedHint); // saved and ran and the clicked button is now disabled
+	// we will need an if else statement here so that we can add and remove classes for the correct and incorrect answers for the score tally
+	if (event.target.classList.contains('correct')) {
+		event.target.classList.remove('btn-primary');
+		event.target.classList.add('btn-success');
+		tallyScore(parseInt(hintPoints.substring(1)));
+		// setTimeout = (target) => {
+		// 	$('#clueModal').modal('hide');
+		// 	event.target.classList.remove('btn-success');
+		// 	event.target.classList.add('btn-primary');
+		// }, 1000, event.target;
+
+	} else {
+		event.target.classList.remove('btn-primary');
+		event.target.classList.add('btn-danger');
+		// tallyScore(-parseInt(hintPoints.substring(1)));
+		tallyScore(-parseInt(hintPoints.substring(1)));
+		// setTimeout = (target) => {
+		// 	$('#clueModal').modal('hide');
+		// 	event.target.classList.remove('btn-danger');
+		// 	event.target.classList.add('btn-primary');
+		// }, 1000, event.target;
+	}
+}
+
+const tallyScore = (e) => {
+	currentScore = currentScore + e;
+	if (currentScore >= 0)
+		updateScore.innerHTML = '$' + currentScore;
+	else
+		updateScore.innerHTML = '-$' + Math.abs(currentScore);
+
+}
+
+// $('#clueModal').on('hidden.bs.modal', () => {
+// 	answer1.classList.remove('correct');
+// 	answer2.classList.remove('correct');
+// 	answer3.classList.remove('correct');
+// 	answer4.classList.remove('correct');
+// })
+
+// const tallyScoreSubtract = (e) => {
+// 	currentScore = currentScore - e;
+//
+// 	updateScore.innerHTML = '$' + currentScore;
+// 	// } else
+// 	// 	updateScore.innerHTML = '$' + Math.abs(currentScore);
+//
+// }
 
 //==============================================
 // variables for modal text updates
@@ -27,86 +172,102 @@
 // questions, point values, and answer options
 //==============================================
 
-let clueText = document.getElementById('clueText');
-let answer1 = document.getElementById('answer1');
-let answer2 = document.getElementById('answer2');
-let answer3 = document.getElementById('answer3');
-let answer4 = document.getElementById('answer4');
-
-//========================
-// game-specific variables
-//========================
-
-let theScore = document.getElementById('theScore'); // updates score in header
-let currentScore = 0;
-// let hintPoints = 0; // point value of clickedHint
-let clickedHint; // this will change throughout the game (e.currentTarget/relatedTarget)
-
-const revealHint = (cluenames, clue) => {
-	getTheClues(cluenames, clue);
-	$('#clueModal').modal('show');
-	// console.log('clicked') checked to see if it's actually working
-}
-
-const getTheClues = (cluenames, clue) => {
-	console.log(cluenames);
-	points.innerHTML = '$' + 200 * clue;
-	clueText.innerHTML = hints['cluenames' + cluenames]['clue' + clue].clueText;
-	answer1.innerHTML = hints['cluenames' + cluenames]['clue' + clue].answer1;
-	answer2.innerHTML = hints['cluenames' + cluenames]['clue' + clue].answer2;
-	answer3.innerHTML = hints['cluenames' + cluenames]['clue' + clue].answer3;
-	answer4.innerHTML = hints['cluenames' + cluenames]['clue' + clue].answer4;
-	findCorrectAnswer(hints['cluenames' + cluenames]['clue' + clue].correct);
-	hintPoints = event.target.innerHTML;
-	clickedHint = event.target;
-}
-
-// using switch statement to compare the value of the expression to the value of each case(aka trying to find a match between correct and the answer option)--break means there wasn't a match so move on: https://www.w3schools.com/js/js_switch.asp
-const findCorrectAnswer = (correct) => {
-	switch (correct) {
-	case '1':
-		answer1.classList.add('correct');
-		break;
-	case '2':
-		answer2.classList.add('correct');
-		break;
-	case '3':
-		answer3.classList.add('correct');
-		break;
-	case '4':
-		answer4.classList.add('correct');
-		break;
-	}
-}
-
-const disable = (e) => {
-	e.classList.remove('clue');
-	e.classList.add('disabled');
-	e.innerHTML = '';
-}
-
-const finalAnswer = () => {
-	disable(clickedHint);
-
-	if (event.target.classList.contains('correct')) {
-		event.target.classList.remove('btn-primary');
-		event.target.classList.add('btn-win');
-		// addScore(parseInt(hintPoints.substring(1)));
-		$('#clueModal').modal('hide');
-		target.classList.remove('btn-win');
-		target.classList.add('btn-primary');
-	} else {
-		event.target.classList.remove('btn-primary');
-		event.target.classList.add('btn-lose');
-		// addScore(-parseInt(hintPoints.substring(1)));
-		$('#clueModal').modal('hide');
-		// target.classList.remove('btn-lose');
-		// target.classList.add('btn-primary');
-	}
-}
-
-// const addScore = (e) => {
-// 	currentScore = currentScore + e;
-// 	if (currentScore >= 0)
+// let clueText = document.getElementById('clueText');
+// let cluePoints = document.getElementById('points');
+// let answer1 = document.getElementById('answer1');
+// let answer2 = document.getElementById('answer2');
+// let answer3 = document.getElementById('answer3');
+// let answer4 = document.getElementById('answer4');
+// let updateScore = document.querySelector('.updateScore');
+//
+// //========================
+// // game-specific variables
+// //========================
+//
+// let clickedHint; // this will change throughout the game (e.currentTarget/relatedTarget)
+// let theScore = 0;
+// let hintPoints = 0;
+//
+// const revealHint = (cluenames, clue) => {
+// 	getTheClues(cluenames, clue);
+// 	$('#clueModal').modal('show');
+// 	// console.log('clicked') checked to see if it's actually working
+// }
+//
+// const getTheClues = (cluenames, clue) => {
+// 	// console.log(cluenames); checked to make sure it was showing the right cluename number from findTheCorrectAnswer
+// 	// numToPoints so that i could add a $ to the points to make it a string on the label
+// 	let numToPoints = hints['cluenames' + cluenames]['clue' + clue].points;
+// 	// console.log(numToPoints)
+// 	cluePoints.innerHTML = '$' + numToPoints;
+// 	// cluePoints.innerHTML = hints['cluenames' + cluenames]['clue' + clue].points;
+// 	clueText.innerHTML = hints['cluenames' + cluenames]['clue' + clue].clueText;
+// 	answer1.innerHTML = hints['cluenames' + cluenames]['clue' + clue].answer1;
+// 	answer2.innerHTML = hints['cluenames' + cluenames]['clue' + clue].answer2;
+// 	answer3.innerHTML = hints['cluenames' + cluenames]['clue' + clue].answer3;
+// 	answer4.innerHTML = hints['cluenames' + cluenames]['clue' + clue].answer4;
+// 	findCorrectAnswer(hints['cluenames' + cluenames]['clue' + clue].correct);
+// 	hintPoints = event.target.innerHTML;
+// 	console.log(hintPoints)
+// 	// let newPoints = parseInt(hintPoints.substring(1))
+// 	// console.log(newPoints)
+// 	clickedHint = event.target;
+// 	// console.log(event.target); making sure i have the right target to generate the score
 //
 // }
+//
+// // using switch statement to compare the value of the expression to the value of each case(aka trying to find a match between correct and the answer option)--break means there wasn't a match so move on: https://www.w3schools.com/js/js_switch.asp
+// const findCorrectAnswer = (correct) => {
+// 	switch (correct) {
+// 	case '1':
+// 		answer1.classList.add('correct');
+// 		break;
+// 	case '2':
+// 		answer2.classList.add('correct');
+// 		break;
+// 	case '3':
+// 		answer3.classList.add('correct');
+// 		break;
+// 	case '4':
+// 		answer4.classList.add('correct');
+// 		break;
+// 	}
+// }
+//
+// const disable = (e) => {
+// 	e.classList.remove('clue');
+// 	e.classList.add('disabled');
+// 	e.innerHTML = '';
+// }
+//
+// const finalAnswer = () => {
+// 	disable(clickedHint);
+// 	if (event.target.classList.contains('correct')) {
+// 		event.target.classList.remove('btn-primary');
+// 		event.target.classList.add('btn-win');
+// 		tallyScore(parseInt(hintPoints.substring(1)));
+// 		// console.log(hintPoints)
+// 		$('#clueModal').modal('hide');
+// 		target.classList.remove('btn-win');
+// 		target.classList.add('btn-primary');
+// 	} else {
+// 		$('#clueModal').modal('hide');
+// 		// target.classList.remove('btn-lose');
+// 		// target.classList.add('btn-primary');
+// 	}
+// }
+//
+// const tallyScore = (e) => {
+// 	score = score + e;
+// 	if (score >= 0)
+// 		updateScore.innerHTML = '$' + score;
+// 	else
+// 		updateScore.innerHTML = '$' + Math.abs(score);
+// }
+//
+// // $('#clueModal').on('hidden.bs.modal', () => {
+// // 	answer1.classList.remove('correct');
+// // 	answer2.classList.remove('correct');
+// // 	answer3.classList.remove('correct');
+// // 	answer4.classList.remove('correct');
+// // });
--- jeopardy/js/hints.js
+++ jeopardy/js/hints.js
@@ -15,8 +15,8 @@ const hints = {
 			answer2: 'What are self-closing tags?',
 			answer3: 'What are elements that require alt-text?',
 			answer4: 'What are elements that should be included in a &lt;div&gt;?',
-			correct: 2,
-			points: 200
+			correct: '2'
+
 		},
 		clue2: {
 			clueText: 'This HTML element attribute is used for something that probably happens once or twice.',
@@ -24,8 +24,8 @@ const hints = {
 			answer2: 'What is unique?',
 			answer3: 'What is an id?',
 			answer4: 'What is a class?',
-			correct: 3,
-			points: 400
+			correct: '3'
+
 		},
 		clue3: {
 			clueText: 'This HTML tag allows a hyperlink.',
@@ -33,8 +33,8 @@ const hints = {
 			answer2: 'What is &lt;link&gt;',
 			answer3: 'What is &lt;src&gt;?',
 			answer4: 'What is &lt;href&gt;?',
-			correct: 1,
-			points: 600
+			correct: '1'
+
 		},
 		clue4: {
 			clueText: 'In HTML, a table is made of these elements.',
@@ -42,8 +42,8 @@ const hints = {
 			answer2: 'What is &lt;tb&gt;, &lt;td&gt;?',
 			answer3: 'What is &lt;table&gt;, &lt;tr&gt;, &lt;th&gt;, &lt;td&gt;?',
 			answer4: 'What is &lt;table&gt;, &lt;h1&gt;, &lt;section&gt;?',
-			correct: 3,
-			points: 800
+			correct: '3'
+
 		},
 		clue5: {
 			clueText: 'This element is an inline container used to mark up only part of a text.',
@@ -51,8 +51,8 @@ const hints = {
 			answer2: 'What is &lt;section&gt;?',
 			answer3: 'What is &lt;span&gt;?',
 			answer4: 'What is &lt;style&gt;?',
-			correct: 3,
-			points: 1000
+			correct: '3'
+
 		}
 	},
 	// css category
@@ -64,8 +64,8 @@ const hints = {
 			answer2: 'What are attributes required for every selector?',
 			answer3: 'What are properties considered necessary for Flexbox?',
 			answer4: 'What are attributes that all mean the same thing?',
-			correct: 1,
-			points: 200
+			correct: '1'
+
 		},
 		clue2: {
 			clueText: 'This CSS property allows you to identify which type of font you\'d like to use.',
@@ -73,8 +73,8 @@ const hints = {
 			answer2: 'What is font-style?',
 			answer3: 'What is text?',
 			answer4: 'What is font-family?',
-			correct: 4,
-			points: 400
+			correct: '4'
+
 		},
 		clue3: {
 			clueText: 'This CSS property improves a browser\'s handling of small windows.',
@@ -82,8 +82,8 @@ const hints = {
 			answer2: 'What is position?',
 			answer3: 'What is max-width?',
 			answer4: 'What is display?',
-			correct: 3,
-			points: 600
+			correct: '3'
+
 		},
 		clue4: {
 			clueText: 'The opacity property specifies the transparency of an element. It takes one of these values.',
@@ -91,8 +91,8 @@ const hints = {
 			answer2: 'What is any integer?',
 			answer3: 'What are positive numbers only?',
 			answer4: 'What are numbers between 0 - 100?',
-			correct: 1,
-			points: 800
+			correct: '1'
+
 		},
 		clue5: {
 			clueText: 'This type of display allows you to set the width, height, margin, and padding of an element, while allowing that element to sit next to other elements.',
@@ -100,8 +100,7 @@ const hints = {
 			answer2: 'What is block?',
 			answer3: 'What is grid?',
 			answer4: 'What is flex?',
-			correct: 1,
-			points: 1000
+			correct: '1'
 		},
 	},
 	// javascript category
@@ -113,8 +112,8 @@ const hints = {
 			answer2: 'What is false?',
 			answer3: 'What is true?',
 			answer4: 'What is the incorrect way to use a boolean?',
-			correct: 3,
-			points: 200
+			correct: '3'
+
 		},
 		clue2: {
 			clueText: 'This type of operator description let\'s you determine whether there\'s a remainder.',
@@ -122,8 +121,8 @@ const hints = {
 			answer2: 'What is division?',
 			answer3: 'What is decrement?',
 			answer4: 'What is combining division and subtraction?',
-			correct: 1,
-			points: 400
+			correct: '1'
+
 		},
 		clue3: {
 			clueText: 'This type of loop is a great choice to use if you already know how many times you need to loop through something.',
@@ -131,8 +130,8 @@ const hints = {
 			answer2: 'What is a For loop?',
 			answer3: 'What is a While loop?',
 			answer4: 'What is a For Of loop?',
-			correct: 2,
-			points: 600
+			correct: '2'
+
 		},
 		clue4: {
 			clueText: 'This JavaScript object can be used to hold multiple objects, including itself.',
@@ -140,8 +139,8 @@ const hints = {
 			answer2: 'What is an object key?',
 			answer3: 'What are key value pairs?',
 			answer4: 'What is an object literal?',
-			correct: 1,
-			points: 800
+			correct: '1'
+
 		},
 		clue5: {
 			clueText: 'In the context of the most recent version of JavaScript, this variable declaration allows you to redeclare the variable later.',
@@ -149,8 +148,8 @@ const hints = {
 			answer2: 'What is const?',
 			answer3: 'What is let?',
 			answer4: 'What are both const and let?',
-			correct: 3,
-			points: 1000
+			correct: '3'
+
 		},
 	},
 	// cli category
@@ -162,8 +161,8 @@ const hints = {
 			answer2: 'What is ls?',
 			answer3: 'What is ls -l?',
 			answer4: 'What is ls ~?',
-			correct: 1,
-			points: 200
+			correct: '1'
+
 		},
 		clue2: {
 			clueText: 'This command removes a directory and everything in it.',
@@ -171,8 +170,8 @@ const hints = {
 			answer2: 'What is rm?',
 			answer3: 'What is rmdir?',
 			answer4: 'What is rm -r?',
-			correct: 4,
-			points: 400
+			correct: '4'
+
 		},
 		clue3: {
 			clueText: 'This command will print your file.',
@@ -180,8 +179,8 @@ const hints = {
 			answer2: 'What is cat?',
 			answer3: 'What is touch-print?',
 			answer4: 'What is print &lt;file&gt;?',
-			correct: 2,
-			points: 600
+			correct: '2'
+
 		},
 		clue4: {
 			clueText: 'This command moves a file or folder into another folder that\'s in the same directory.',
@@ -189,8 +188,8 @@ const hints = {
 			answer2: 'What is cp?',
 			answer3: 'What is mvdir?',
 			answer4: 'What is mv -dir?',
-			correct: 1,
-			points: 800
+			correct: '1'
+
 		},
 		clue5: {
 			clueText: 'To infinity and beyond! This command stops a program from running when you\'ve written an infinite loop.',
@@ -198,8 +197,8 @@ const hints = {
 			answer2: 'What is CTRL ALT DELETE?',
 			answer3: 'What is CMD F?',
 			answer4: 'What is ESC?',
-			correct: 1,
-			points: 1000
+			correct: '1'
+
 		},
 	},
 	// github category
@@ -211,8 +210,8 @@ const hints = {
 			answer2: 'What is git status?',
 			answer3: 'What is git help?',
 			answer4: 'What is git init?',
-			correct: 2,
-			points: 200
+			correct: '2'
+
 		},
 		clue2: {
 			clueText: 'This is one way to turn an existing directory into a Git repository.',
@@ -220,8 +219,8 @@ const hints = {
 			answer2: 'What is git --setup &lt;file&gt;',
 			answer3: 'What is git clone (URL)?',
 			answer4: 'What is git log?',
-			correct: 1,
-			points: 400
+			correct: '1'
+
 		},
 		clue3: {
 			clueText: 'This command will grab all the commits from the remote branch and merge them into your own branch.',
@@ -229,8 +228,8 @@ const hints = {
 			answer2: 'What is fetch?',
 			answer3: 'What is merge?',
 			answer4: 'What is pull?',
-			correct: 4,
-			points: 600
+			correct: '4'
+
 		},
 		clue4: {
 			clueText: 'This command is important when considering version control.',
@@ -238,8 +237,8 @@ const hints = {
 			answer2: 'What is git config?',
 			answer3: 'What is git commit?',
 			answer4: 'What is git push?',
-			correct: 3,
-			points: 800
+			correct: '3'
+
 		},
 		clue5: {
 			clueText: 'This request alerts the original owner of a repository that you\'ve made changes to their code that might be beneficial.',
@@ -247,8 +246,8 @@ const hints = {
 			answer2: 'What is a fork request?',
 			answer3: 'What is a pull request?',
 			answer4: 'What is a commit request?',
-			correct: 3,
-			points: 1000
+			correct: '3'
+
 		},
 	},
 	// lolz category
@@ -260,8 +259,8 @@ const hints = {
 			answer2: 'What is Rubber Duck coding?',
 			answer3: 'What is a catchy tune?',
 			answer4: 'What is something that doesn\'t make sense to me?',
-			correct: 2,
-			points: 200
+			correct: '2'
+
 		},
 		clue2: {
 			clueText: 'In the context of programming, they don\'t die, they multiply!',
@@ -269,8 +268,8 @@ const hints = {
 			answer2: 'What are bugs?',
 			answer3: 'What are chores?',
 			answer4: 'What are breakpoints?',
-			correct: 2,
-			points: 400
+			correct: '2'
+
 		},
 		clue3: {
 			clueText: 'Regina George would NOT be happy about this GitHub term... so stop trying to make it happen!',
@@ -278,8 +277,8 @@ const hints = {
 			answer2: 'What is merge?',
 			answer3: 'What is upstream?',
 			answer4: 'What is master?',
-			correct: 1,
-			points: 600
+			correct: '1'
+
 		},
 		clue4: {
 			clueText: 'This character has been the hide and seek champion since 1958.',
@@ -287,8 +286,8 @@ const hints = {
 			answer2: 'What is the closing curly bracket?',
 			answer3: 'What is the semicolon?',
 			answer4: 'What is the dollar sign?',
-			correct: 3,
-			points: 800
+			correct: '3'
+
 		},
 		clue5: {
 			clueText: 'This word is used by programmers when they don\'t want to explain what they just did. Richard Hendricks supposedly wrote one with 10 parentheses.',
@@ -296,8 +295,8 @@ const hints = {
 			answer2: 'What is a nest arrow function?',
 			answer3: 'What is a nested loop?',
 			answer4: 'What is a RegExp?',
-			correct: 1,
-			points: 1000
+			correct: '1'
+
 		},
 	}
 }
